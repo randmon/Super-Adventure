86a6add198c55c1e81908bdcf9a3133e66132f05
randmon <ccmarques98@gmail.com>
2020-02-24 02:14:15 +0100

    moved game logic functions from UI project to Engine project, added new message handling class

---
+++ Engine/MessageEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class MessageEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+        public bool AddExtraNewLine { get; private set; }
+
+        public MessageEventArgs(string message, bool addExtraNewLine)
+        {
+            Message = message;
+            AddExtraNewLine = addExtraNewLine;
+        }
+    }
+}
